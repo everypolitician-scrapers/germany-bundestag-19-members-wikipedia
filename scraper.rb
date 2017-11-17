16eb34dc41173bd48dc3e87c33b5bafa143cc80d
Henare Degan <henare.degan@gmail.com>
2017-11-18 10:51:43 +1100

    Initial commit of scraper skeleton

---
+++ scraper.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'pry'
+require 'scraped'
+require 'scraperwiki'
+require 'wikidata_ids_decorator'
+
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
