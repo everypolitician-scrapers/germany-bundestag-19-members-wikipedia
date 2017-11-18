010ce4ba4b7a5c0282fc03400f52f2eea3392ccf
Henare Degan <henare.degan@gmail.com>
2017-11-18 11:54:17 +1100

    Add scraper for 19th German Bundestag

--- scraper.rb
+++ scraper.rb
@@ -8,3 +8,68 @@ require 'wikidata_ids_decorator'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
+
+class MembersPage < Scraped::HTML
+  decorator WikidataIdsDecorator::Links
+
+  field :members do
+    table.xpath('.//tr[td]').map do |tr|
+      fragment tr => MemberRow
+    end
+  end
+
+  private
+
+  def table
+    noko.xpath(".//table[.//th[contains(.,'Mitglied')]]").first
+  end
+end
+
+class MemberRow < Scraped::HTML
+  field :name do
+    tds[1].text
+  end
+
+  field :sort_name do
+    tds.css('td/@data-sort-value').text
+  end
+
+  field :wikidata do
+    tds[1].css('a/@wikidata').text
+  end
+
+  field :birth_year do
+    tds[2].text
+  end
+
+  field :party do
+    tds[3].text
+  end
+
+  field :constituency do
+    tds[5].text
+  end
+
+  field :constituency_wikidata do
+    # TODO: Would be nicer to use something like Rails' `#try` here
+    tds[5].css('a/@wikidata').text rescue nil
+  end
+
+  field :term do
+    19
+  end
+
+  private
+
+  def tds
+    noko.css('td')
+  end
+end
+
+url = 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_Deutschen_Bundestages_(19._Wahlperiode)'
+page = MembersPage.new(response: Scraped::Request.new(url: url).response)
+data = page.members.map(&:to_h).map { |m| m.reject { |_, v| v.to_s.empty? } }
+
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+ScraperWiki.save_sqlite(%i[name term], data)
